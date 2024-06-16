2333af095adfe3694749aaf8b0b65d6474666f92
Kia <kia@wattpad.com>
2024-06-16 17:09:40 -0400

    Update stories_author/app.py

--- src/stories_author/app.py
+++ src/stories_author/app.py
@@ -1,3 +1,8 @@
+"""
+This is the public version of `app.py` - that will be made available in the public git repos.
+
+The candidate will be asked to make improvements.
+"""
 import csv
 from datetime import date
 from pathlib import Path
@@ -22,40 +27,37 @@ def transform(
     The result is then outputted as CSV to output_stories_author_csv_p [models.StoryAuthor].
     """
 
+    # Read input csv files
     with stories_csv_p.open() as stories_csv_f, \
-            users_csv_p.open() as users_csv_f, \
-            output_stories_author_csv_p.open('w') as output_f:
-
-        output_w = csv.DictWriter(output_f, models.StoryAuthor.model_fields.keys())
-        output_w.writeheader()
-
-        # List used to track rows that will be outputted
-        output_rows = []
+            users_csv_p.open() as users_csv_f:
 
         stories_csv_r = csv.DictReader(stories_csv_f)
-        for story_row in stories_csv_r:
-            story = models.Story(**story_row)
+        story_models = [models.Story(**row) for row in stories_csv_r]
 
-            if story.modified_ts.date() != modified_dt:
-                continue
+        users_csv_r = csv.DictReader(users_csv_f)
+        user_models = [models.User(**row) for row in users_csv_r]
 
-            # We use a brute-force linear search of the users_csv to find the matching
-            # user.id to join with the story.author_id. We therefore need to
-            # reset the users_csv_f location to the beginning of the file.
-            users_csv_f.seek(0)
-            users_csv_r = csv.DictReader(users_csv_f)
+    # List used to track rows that will be outputted
+    output_rows = []
 
-            for user_row in users_csv_r:
-                user = models.User(**user_row)
-                if user.id == story.author_id:
-                    story_author = models.StoryAuthor(
-                        id=story.id,
-                        title=story.title,
-                        summary=story.summary,
-                        author_id=user.id,
-                        author_name=user.name,
-                        author_profile=user.profile,
-                        author_email=user.email)
-                    output_rows.append(dict(story_author))
+    for story in story_models:
+        if story.modified_ts.date() != modified_dt:
+            continue
 
+        for user in user_models:
+            if user.id == story.author_id:
+                story_author = models.StoryAuthor(
+                    id=story.id,
+                    title=story.title,
+                    summary=story.summary,
+                    author_id=user.id,
+                    author_name=user.name,
+                    author_profile=user.profile,
+                    author_email=user.email)
+                output_rows.append(dict(story_author))
+
+    # Save output_rows to output csv file
+    with output_stories_author_csv_p.open('w') as output_f:
+        output_w = csv.DictWriter(output_f, models.StoryAuthor.model_fields.keys())
+        output_w.writeheader()
         output_w.writerows(output_rows)
